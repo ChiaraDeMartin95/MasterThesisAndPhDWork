b67cc349e1a8a26d967c7e66c00b9367d5c3c9b7
chiarademartin <chiara.demartin.keira@gmail.com>
2020-03-14 16:58:46 +0100

    pippo changes

---
+++ pippo.C
@@ -0,0 +1,25 @@
+#include <TLine.h>
+#include "Riostream.h"
+#include "TTimer.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include <TH3F.h>
+#include "TNtuple.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TF1.h"
+#include "TProfile.h"
+#include <TTree.h>
+#include <TLatex.h>
+#include <TFile.h>
+#include <TLegend.h>
+
+void pippo(){
+
+  questo file serve a fare delle prove su git
+}
+
