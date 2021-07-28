f6b684a67bbc982af66afd5abc392a8fa2f87922
chiarademartin <chiara.demartin.keira@gmail.com>
2021-07-28 17:58:22 +0200

    updated

--- hK0sTrigger.cxx
+++ hK0sTrigger.cxx
@@ -22,7 +22,6 @@
 #include "AnalysisDataModel/EventSelection.h"
 #include "AnalysisDataModel/Centrality.h"
 #include "AnalysisDataModel/PID/PIDResponse.h"
-//#include "filterTables.h"
 #include "Framework/HistogramRegistry.h"
 
 #include <TFile.h>
@@ -43,13 +42,16 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 using std::array;
 
-const float massK0s = 0.497611;
+const float ctauK0s = 2.6844;   //from PDG                                                                                                    
+const float massK0s = 0.497611; //from PDG    
 
 struct hK0sTrigger {
 
   //Produces<aod::hK0sTriggers> tags;
 
-  HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject};
+  HistogramRegistry histos{"Histos", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry histostracks{"HistosTracks", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
+  HistogramRegistry histosK0s{"HistosK0s", {}, OutputObjHandlingPolicy::AnalysisObject, true, true};
 
   Configurable<float> cfgCutVertex{"cfgCutVertex", 10.0f, "Accepted z-vertex range"};
   Configurable<float> cfgCutEta{"cfgCutEta", 0.8f, "Eta range for tracks"};
@@ -65,7 +67,9 @@ struct hK0sTrigger {
   Configurable<float> eta{"eta", 0.8, "eta"}; //
   Configurable<float> ctau{"ctau", 20, "ctau"};
   Configurable<float> LRej{"LRej", 0.005, "LRej"}; //
-  Configurable<float> minpt{"minpt", 1.0, "minpt"}; //
+  Configurable<float> minpt{"minpt", 0, "minpt"}; //
+  Configurable<int> properlifetimefactor{"properlifetimefactor", 5, "Proper Lifetime cut"};
+  Configurable<float> nsigmatpc{"nsigmatpc", 6, "N Sigmas TPC"};
 
   void init(o2::framework::InitContext&)
   {
@@ -82,20 +86,24 @@ struct hK0sTrigger {
     histos.add("CentralityFullTrigger", "Centrality distribution of events with h + Xi (V0M)", kTH1F, {centAxis});
     histos.add("VtxZAfterSel", "Vertex distribution in Z;Z (cm)", kTH1F, {vtxZAxis});
     histos.add("fProcessedEvents", "Selected events", HistType::kTH1F, {{4, 0.5, 4.5, "Event counter"}});
-    histos.add("PtTrigger", "PtTrigger",  HistType::kTH1F, {{300, 0, 30, "Pt of trigger particle"}});
-    histos.add("hMassK0ShortBefSel", "hMassK0ShortBefSel", kTH1F, {massK0sAxis});
-    histos.add("hMassK0ShortAfterSel", "hMassK0ShortAfterSel", kTH1F, {massK0sAxis});
 
-    histos.add("hptK0s", "hptK0s", kTH1F, {{100, 0, 10}});
-    histos.add("hctau", "hctau", kTH1F, {{250, 0, 25}});
-    histos.add("hdcav0dau", "hdcav0dau", kTH1F, {{20, 0, 2}});
-    histos.add("hdcanegtopv", "hdcanegtopv", kTH1F, {{10, 0, 0.1}});
-    histos.add("hdcapostopv", "hdcapostopv", kTH1F, {{10, 0, 0.1}}); 
-    histos.add("hdcav0topv", "hdcav0topv", kTH1F, {{50, 0, 1}});
-    histos.add("hv0cospa", "hv0cospa", kTH1F, {{50,0.95, 1}});
-    histos.add("hv0radius", "hv0radius", kTH1F, {{100, 0, 5}});
-    histos.add("hrapidity", "hrapidity", kTH1F, {{100, -1, 1}});
-    histos.add("heta", "heta", kTH1F, {{100, -1, 1}});
+    histostracks.add("hTriggeredParticles", "Selected triggered particles", HistType::kTH1F, {{10, 0.5, 10.5, "Trigger counter"}});
+    histostracks.add("hLengthvsCrossedRows", "hLengthvsCrossedRows", kTH2F, {{300, 0, 300}, {150, 0, 150}});
+    histostracks.add("hTrackLength", "hTrackLength", kTH1F, {{1000, 0, 1000}});
+    histostracks.add("PtTrigger", "PtTrigger",  HistType::kTH1F, {{300, 0, 30, "Pt of trigger particle"}});
+
+    histosK0s.add("hMassK0ShortBefSel", "hMassK0ShortBefSel", kTH1F, {massK0sAxis});
+    histosK0s.add("hMassK0ShortAfterSel", "hMassK0ShortAfterSel", kTH1F, {massK0sAxis});
+    histosK0s.add("hptK0s", "hptK0s", kTH1F, {{100, 0, 10}});
+    histosK0s.add("hctau", "hctau", kTH1F, {{250, 0, 25}});
+    histosK0s.add("hdcav0dau", "hdcav0dau", kTH1F, {{20, 0, 2}});
+    histosK0s.add("hdcanegtopv", "hdcanegtopv", kTH1F, {{10, 0, 0.1}});
+    histosK0s.add("hdcapostopv", "hdcapostopv", kTH1F, {{10, 0, 0.1}}); 
+    histosK0s.add("hdcav0topv", "hdcav0topv", kTH1F, {{50, 0, 1}});
+    histosK0s.add("hv0cospa", "hv0cospa", kTH1F, {{50,0.95, 1}});
+    histosK0s.add("hv0radius", "hv0radius", kTH1F, {{100, 0, 5}});
+    histosK0s.add("hrapidity", "hrapidity", kTH1F, {{100, -1, 1}});
+    histosK0s.add("heta", "heta", kTH1F, {{100, -1, 1}});
 
   }
 
@@ -103,7 +111,6 @@ struct hK0sTrigger {
   //other collision selections: no pile up events
   
   Filter trackFilter = (nabs(aod::track::eta) < cfgCutEta) && (aod::track::isGlobalTrack == static_cast<uint8_t>(1u)) && (aod::track::pt>cfgCutMinPt);
-  //other selections for trigger particles: quality criteria, |DCAz| < , |DCAxy| < 
 
   Filter preFilterV0 = nabs(aod::v0data::dcapostopv) > dcapostopv&& nabs(aod::v0data::dcanegtopv) > dcanegtopv   && aod::v0data::dcaV0daughters < dcav0dau;
 
@@ -131,7 +138,22 @@ struct hK0sTrigger {
     histos.fill(HIST("fProcessedEvents"), 2);
 
     for (auto track : tracks) { // start loop over tracks
-      histos.fill(HIST("PtTrigger"), track.pt());
+      histostracks.fill(HIST("hTriggeredParticles"), 1);
+      /*
+      if (track.tpcChi2NCl()>4) continue; 
+      histostracks.fill(HIST("hTriggeredParticles"), 2);
+      if (track.tpcNClsCrossedRows() < 80) continue; //~no changes
+      histostracks.fill(HIST("hTriggeredParticles"), 3);
+      if (track.tpcCrossedRowsOverFindableCls() < 0.8) continue; //no changes
+      histostracks.fill(HIST("hTriggeredParticles"), 4);
+      histostracks.fill(HIST("hTrackLength"), track.length());
+      histostracks.fill(HIST("hLengthvsCrossedRows"), track.tpcNClsCrossedRows(), track.length());
+      //if (track.length() < 90) continue; //some decrease observed, is the method correct? I get lenghts =0 or 370 < l < 550...
+      histostracks.fill(HIST("hTriggeredParticles"), 5);
+      //if (float(track.tpcNClsCrossedRows())/track.length() < 0.8) continue; //no track passes this selection!
+      histostracks.fill(HIST("hTriggeredParticles"), 6);
+      */
+      histostracks.fill(HIST("PtTrigger"), track.pt());
       keepEvent = true; // a trigger particle is found
     } // end loop over tracks
 
@@ -139,12 +161,24 @@ struct hK0sTrigger {
     histos.fill(HIST("fProcessedEvents"), 3);
     histos.fill(HIST("CentralityTriggered"), collision.centV0M());
 
-    for (auto& v0 : fullV0s) {
-      histos.fill(HIST("hMassK0ShortBefSel"), v0.mK0Short());
-      //      ctauK0s = massK0s * sqrt(pow(v0.x(), 2)+ pow(v0.y(), 2)+ pow(v0.z(), 2)) / v0.p();
+    keepEvent=false;
+    float v0pos = -1.;
+    float K0sproperlifetime = -1.;
+    float v0ptotmom = -1.;
 
-      if (TMath::Abs(v0.posTrack_as<DaughterTracks>().tpcNSigmaPi()) > 3.0) continue;
-      if (TMath::Abs(v0.negTrack_as<DaughterTracks>().tpcNSigmaPi()) > 3.0) continue; 
+    for (auto& v0 : fullV0s) {
+
+      //Position                                                                     
+      v0pos = TMath::Sqrt(TMath::Power(v0.x() - collision.posX(), 2) + TMath::Power(v0.y() - collision.posY(), 2) + TMath::Power(v0.z() - collision.posZ(), 2));
+      //Total momentum                                                                                                                           
+      v0ptotmom = TMath::Sqrt(v0.px() * v0.px() + v0.py() * v0.py() + v0.pz() * v0.pz());
+      //Proper lifetime                                                                                                                          
+      K0sproperlifetime = massK0s * v0pos / (v0ptotmom + 1e-13);
+
+      histosK0s.fill(HIST("hMassK0ShortBefSel"), v0.mK0Short());
+
+      if (TMath::Abs(v0.posTrack_as<DaughterTracks>().tpcNSigmaPi()) > nsigmatpc) continue;
+      if (TMath::Abs(v0.negTrack_as<DaughterTracks>().tpcNSigmaPi()) > nsigmatpc) continue; 
       //-----------------------
       // TOPOLOGICAL - KINEMATIC SELECTIONS
       //-----------------------
@@ -154,19 +188,20 @@ struct hK0sTrigger {
       if (v0.dcav0topv(collision.posX(), collision.posY(), collision.posZ()) > dcav0topv) continue;
       if (v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) < v0cospa) continue;
       if (TMath::Abs(v0.mLambda() - constants::physics::MassLambda) < LRej) continue;
-      //      if (ctauK0s > 20) continue;
+      if (K0sproperlifetime > properlifetimefactor * ctauK0s) continue;
+      //OOB pile up: tofSignal or HasTOF
 
-      histos.fill(HIST("hptK0s"), v0.pt());
-      //      histos.fill(HIST("hctau"), ctauK0s);
-      histos.fill(HIST("hMassK0ShortAfterSel"), v0.mK0Short());
-      histos.fill(HIST("hdcav0dau"), v0.dcaV0daughters());
-      histos.fill(HIST("hdcanegtopv"), v0.dcanegtopv());
-      histos.fill(HIST("hdcapostopv"), v0.dcapostopv());
-      histos.fill(HIST("hdcav0topv"), v0.dcav0topv(collision.posX(), collision.posY(), collision.posZ()));
-      histos.fill(HIST("hv0cospa"), v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()));
-      histos.fill(HIST("hv0radius"), v0.v0radius());
-      histos.fill(HIST("hrapidity"), v0.yK0Short());
-      histos.fill(HIST("heta"), v0.eta());
+      histosK0s.fill(HIST("hptK0s"), v0.pt());
+      histosK0s.fill(HIST("hctau"), K0sproperlifetime);
+      histosK0s.fill(HIST("hMassK0ShortAfterSel"), v0.mK0Short());
+      histosK0s.fill(HIST("hdcav0dau"), v0.dcaV0daughters());
+      histosK0s.fill(HIST("hdcanegtopv"), v0.dcanegtopv());
+      histosK0s.fill(HIST("hdcapostopv"), v0.dcapostopv());
+      histosK0s.fill(HIST("hdcav0topv"), v0.dcav0topv(collision.posX(), collision.posY(), collision.posZ()));
+      histosK0s.fill(HIST("hv0cospa"), v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()));
+      histosK0s.fill(HIST("hv0radius"), v0.v0radius());
+      histosK0s.fill(HIST("hrapidity"), v0.yK0Short());
+      histosK0s.fill(HIST("heta"), v0.eta());
 
       keepEvent = true;
     }
@@ -175,7 +210,6 @@ struct hK0sTrigger {
       histos.fill(HIST("CentralityFullTrigger"), collision.centV0M());
       histos.fill(HIST("fProcessedEvents"), 4);
     }
-    //    tags(keepEvent);
   }
 };
 
