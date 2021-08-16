72244a7e1f60a3a2e46eece391f2ebd2d4be9c58
chiarademartin <chiara.demartin.keira@gmail.com>
2021-08-16 17:53:52 +0200

    Add reference operator

--- BarlowVariable.C
+++ BarlowVariable.C
@@ -1,7 +1,7 @@
 #include "Riostream.h"
 #include "TH1F.h"
 
-void BarlowVariable(TH1F* hVar, TH1F* hDefault, TH1F* hBarlowVar, TH1F* hSyst, Float_t NSigma, Int_t NSign, Bool_t IsBarlowSign){
+void BarlowVariable(TH1F* hVar, TH1F* hDefault, TH1F* hBarlowVar, TH1F* hSyst, Float_t NSigma, Int_t NSign, Bool_t& IsBarlowSign){
 
   Float_t BarlowVar=0;
   Int_t NBarlowSign=0;
@@ -11,6 +11,7 @@ void BarlowVariable(TH1F* hVar, TH1F* hDefault, TH1F* hBarlowVar, TH1F* hSyst, F
   //  if (hVar->GetNbinsX() != hSyst->GetNbinsX()) return;
 
   for (Int_t b=1; b<=hVar->GetNbinsX();b++){
+
     if (hVar->GetBinContent(b) ==0 || hDefault->GetBinContent(b) ==0 || hVar->GetBinError(b) ==0 || hDefault->GetBinError(b) ==0 || (hVar->GetBinError(b) ==  hDefault->GetBinError(b)) ){
       hBarlowVar->SetBinContent(b,0);
       hBarlowVar->SetBinError(b,0);      
@@ -19,12 +20,14 @@ void BarlowVariable(TH1F* hVar, TH1F* hDefault, TH1F* hBarlowVar, TH1F* hSyst, F
     BarlowVar = (hVar->GetBinContent(b) - hDefault->GetBinContent(b)) / sqrt(TMath::Abs(pow(hVar->GetBinError(b), 2)  - pow( hDefault->GetBinError(b),2) ));
     hBarlowVar->SetBinContent(b,BarlowVar);
     hBarlowVar->SetBinError(b,0);
-    //    cout << hVar->GetBinContent(b) << " " << hDefault->GetBinContent(b) << " " << hVar->GetBinError(b) << " " << hDefault->GetBinError(b) << endl;
-    //    cout << "b " << b << " " << BarlowVar << endl;
+    //cout << hVar->GetBinContent(b) << " " << hDefault->GetBinContent(b) << " " << hVar->GetBinError(b) << " " << hDefault->GetBinError(b) << endl;
+    //cout << "b " << b << " " << BarlowVar << endl;
     if (TMath::Abs(BarlowVar) > NSigma) NBarlowSign++;
   }
   if (NBarlowSign >= NSign) IsBarlowSign=kTRUE;
-  if (IsBarlowSign)  hBarlowVar->SetMarkerStyle(20);
+  if (IsBarlowSign) {
+    hBarlowVar->SetMarkerStyle(24);
+  }
   else hBarlowVar->SetMarkerStyle(33);
 
   //---set syst uncertainty--------
@@ -34,7 +37,13 @@ void BarlowVariable(TH1F* hVar, TH1F* hDefault, TH1F* hBarlowVar, TH1F* hSyst, F
       hSyst->SetBinError(b,0);
       continue;
     }
-    hSyst-> SetBinContent(b, (TMath::Abs(hVar->GetBinContent(b) - hDefault->GetBinContent(b))/2) / hDefault->GetBinContent(b));
-    hSyst->SetBinError(b,0);
+    if (IsBarlowSign){
+      hSyst-> SetBinContent(b, (TMath::Abs(hVar->GetBinContent(b) - hDefault->GetBinContent(b))/2) / hDefault->GetBinContent(b));
+      hSyst->SetBinError(b,0);
+    }
+    else {
+      hSyst-> SetBinContent(b, 0);
+      hSyst->SetBinError(b,0);
+    }
   }
 }
